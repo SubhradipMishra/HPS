5de2b9bc5f50c4d5fdc262b901eaed4f58900990
Subhradip Mishra <mishrasubhradip2005@gmail.com>
2026-04-30 12:40:52 +0530

    Deploy backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Simple Base API Instance
 const API = axios.create({
-  baseURL: "http://localhost:7070",
+  baseURL: "https://hps-j8xl.onrender.com",
   withCredentials: true, // needed for AuthToken cookie
 });
 
